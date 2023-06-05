212bac1af6829aab807890ba0b1ee91fced9abc1
Aubj1202 <114889604+Aubj1202@users.noreply.github.com>
2023-06-05 13:58:45 -0400

    getPreferences

---
+++ Back/src/main/java/ca/usherbrooke/gegi/server/business/Preference.java
@@ -0,0 +1,10 @@
+package ca.usherbrooke.gegi.server.business;
+
+public class Preference {
+    public Integer preference_id;
+    public String nom;
+
+    public String toString() {
+        return nom;
+    }
+}
