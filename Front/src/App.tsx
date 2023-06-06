bc794752848516321e07abdf781db2640e87a2ab
Aubj1202 <114889604+Aubj1202@users.noreply.github.com>
2023-06-06 18:06:34 -0400

    get preferences

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -8,15 +8,25 @@ import BasicModal from './components/BasicModal.js';
 import { Preference } from './components/interfaces';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [preferences, setPreferences] = useState<Preference[]>()
+  const [preferences, setPreference] = useState<Preference[]>([])
   
   useEffect(() => {
     // declare the data fetching function
     const fetchData = async () => {
-      const result = await APIRequest<Preference>("/getPreferences","GET",true);
-      
+      const result = await APIRequest<[]>("/getPreferences","GET",true);
       console.log(result);
+      if (result.data)
+      {
+        result.data.forEach((element:Preference) => {
+          console.log(element);
+          let preference = {preference_id: element.preference_id, nom: element.nom}
+          console.log(preference)
+  
+          setPreference([...preferences, preference]);
+        });
+      }
+      
+      console.log(preferences);
     }
   
     // call the function
@@ -27,31 +37,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <button onClick={() => logout()}>Déconnexion</button>
-      <div>
-        <button onClick={() => requestForStudent()}>Étudiant?</button>
-        <button onClick={() => requestForTeacher()}>Enseignant?</button>
-        <div id="title">
-          <span></span>
+      <nav>
+        <div className='modules'>
+          <p>Émile</p>
         </div>
-        <BasicModal preferences={preferences}/>
+        <h1>SchedulUS</h1>
+        <div className='modules'>
+          <p>Kenza</p>
+          <BasicModal preferences={preferences}/>
+        </div>
+      </nav>
+      <div>
+        
       </div>
     </>
   )
