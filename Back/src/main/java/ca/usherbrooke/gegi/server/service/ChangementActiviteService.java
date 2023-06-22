f4369af7284b221467696ec1625533040b395b30
Mathieu Sevegny <sevegny.mathieu@gmail.com>
2023-06-22 12:38:40 -0400

    Ajout du changement avec un étudiant ou un fantome

--- Back/src/main/java/ca/usherbrooke/gegi/server/service/ChangementActiviteService.java
+++ Back/src/main/java/ca/usherbrooke/gegi/server/service/ChangementActiviteService.java
@@ -1,5 +1,6 @@
 package ca.usherbrooke.gegi.server.service;
 
+import ca.usherbrooke.gegi.server.business.EtudiantEchange;
 import ca.usherbrooke.gegi.server.persistence.ChangementActiviteMapper;
 
 import javax.annotation.security.RolesAllowed;
@@ -17,6 +18,39 @@ public class ChangementActiviteService {
     SecurityContext securityContext;
     @Inject
     ChangementActiviteMapper changementActiviteMapper;
+    private void ChangementAvecFantome(String cip, int activiteId)
+    {
+        Integer oldActiviteId = changementActiviteMapper.getCurrentGroupOfStudent(cip,activiteId);
+        changementActiviteMapper.changerGroupe(cip,oldActiviteId,activiteId);
+    }
+    private void ChangementAvecEtudiant(String cip, int activiteId, EtudiantEchange autreEtudiant)
+    {
+        changementActiviteMapper.supprimerDemandeChangement(autreEtudiant.cip,autreEtudiant.activiteVoulueId);
+        changementActiviteMapper.changerGroupe(cip,autreEtudiant.activiteVoulueId,activiteId);
+        changementActiviteMapper.changerGroupe(autreEtudiant.cip,activiteId,autreEtudiant.activiteVoulueId);
+    }
+    private void EffectuerChangement(String cip, int activiteId)
+    {
+        EtudiantEchange autreEtudiantAEchanger = changementActiviteMapper.getEtudiantVoulantChanger(cip,activiteId);
 
+        //Si null c'est un changement avec un étudiant fantôme
+        if (autreEtudiantAEchanger == null)
+        {
+            ChangementAvecFantome(cip,activiteId);
+        }
+        else
+        {
+            ChangementAvecEtudiant(cip,activiteId,autreEtudiantAEchanger);
+        }
+    }
 
+    @GET
+    @Path("testEffectuerChangement")
+    public String testEffectuerChangement()
+    {
+        String cip = "stds2101";
+        int activite = 11;
+        EffectuerChangement(cip,activite);
+        return "Regarde la bd";
+    }
 }
