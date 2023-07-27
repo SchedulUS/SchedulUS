06a1f2ec782e9df4472ff4043f88ba42bc8f2269
Tristan Lafontaine <lafontaine.tristan@carrefour.cegepvicto.ca>
2023-07-27 12:24:41 -0400

    Fixe bug intendant et menage du code en commentaire

--- Back/src/main/java/ca/usherbrooke/gegi/server/persistence/PreferenceMapper.java
+++ Back/src/main/java/ca/usherbrooke/gegi/server/persistence/PreferenceMapper.java
@@ -15,12 +15,6 @@ public interface PreferenceMapper
     void setPreferenceUsagerAPP(@Param("cip") String cip, @Param("appId") int appId,@Param("preferenceId") int preferenceId, @Param("intendant") boolean intendant);
     Integer getPreferenceUsager(String cip);
     Integer getPreferenceUsagerAPP(@Param("cip") String cip, @Param("appId") int appId);
-/*
-    List<Message> select(String trimesterId, String profileId, String unit, Integer id);
-    Message selectOne(Integer id);
-    void deleteOne(Integer id);
-    List<Message> allMessages();
-    void insertMessage(@Param("message") Message message);
-    Integer getNewId();
-*/
+    Boolean getRowExistPreferenceUsagerAPP(@Param("cip") String cip, @Param("appId") int appId);
+    void updateIntendant(@Param("cip") String cip, @Param("appId") int appId,@Param("preferenceId") int preferenceId, @Param("intendant") boolean intendant);
 }
--- Back/src/main/java/ca/usherbrooke/gegi/server/persistence/UsagerMapper.java
+++ Back/src/main/java/ca/usherbrooke/gegi/server/persistence/UsagerMapper.java
@@ -11,12 +11,4 @@ public interface UsagerMapper
 {
     Person getNbrEchange(String cip);
     void setPreference(@Param("cip") String cip, @Param("preferenceId") int preferenceId);
-/*
-    List<Message> select(String trimesterId, String profileId, String unit, Integer id);
-    Message selectOne(Integer id);
-    void deleteOne(Integer id);
-    List<Message> allMessages();
-    void insertMessage(@Param("message") Message message);
-    Integer getNewId();
-*/
 }
